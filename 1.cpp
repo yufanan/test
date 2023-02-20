a5167c327a618f4ce8f4dd0216094d4f916cc8a9
AnYufan <2354774262@qq.com>
2023-02-20 20:31:42 +0800

    test

---
+++ 1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int sum(int * ar, int n) // 更通用的方法
+{ int
+i;
+int total = 0;
+for (i = 0; i < n; i++) // 使用 n 个元素
+total += ar[i]; // ar[i] 和 *(ar + i) 相同
+return total;
+}
+
+int main()
+{
+	int a;
+	
+	sum(&a,3)
+}
+
+
